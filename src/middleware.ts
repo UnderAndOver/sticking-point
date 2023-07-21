32464d966cb47d4a48d1a0f291ca74b481263cca
UnderAndOver <18629496+UnderAndOver@users.noreply.github.com>
2023-07-21 04:50:29 -0500

    Develop (#7)
    * quick middleware fix
    
    * chore(middleware)test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,19 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const excludedPaths = [
-    "/_next/",
-    "/static/",
-    "/",
-    "/logo.png",
-    "/favicon.ico",
-  ]; // add more paths here if needed
+  const excludedPaths = ["_next", "static", "", "logo.png", "favicon.ico"]; // add more paths here if needed
+  
+  // Get the first segment of the path
+  const firstSegment = req.nextUrl.pathname.split("/")[1];
 
-  // Check if the request is for any excluded path
-  if (excludedPaths.some((path) => req.nextUrl.pathname.startsWith(path))) {
+  // Check if the first segment is an excluded path
+  if (excludedPaths.includes(firstSegment)) {
     // If it is, do nothing and let the request continue
     return NextResponse.next();
   }
 
-  return NextResponse.redirect("https://www.stickingpoint.co/");
+  const url = req.nextUrl.clone();
+  // If the first segment is not an excluded path, redirect to '/'
+  url.pathname = "/";
+  return NextResponse.redirect(url.toString());
 }
